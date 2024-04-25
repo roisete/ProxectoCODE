93d7580b72396f919ada70fd84b88c22fe1b4bbd
Roi <roi4444efp@gmail.com>
2024-04-25 10:17:04 +0200

    Cuarto commit - Cambios menores na clase Ruleta

--- src/Ruleta.java
+++ src/Ruleta.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class Ruleta {
     private int [] ruleta;
-    private final int TAMANHO=37;
     private int casilla;
     private String color;
 
@@ -9,7 +8,7 @@ public class Ruleta {
      * Constructor de ruleta
      */
     public Ruleta() {
-        this.ruleta= new int[TAMANHO];
+        this.ruleta= new int[37];
     }
 
     /**
@@ -23,35 +22,51 @@ public class Ruleta {
         }
     }
 
+    /**
+     * Getter de variable casilla
+     * @return casilla
+     */
     public int getCasilla() {
         return casilla;
     }
 
-
+    /**
+     * Setter de casilla
+     */
     public void setCasilla() {
         Random r= new Random();
         this.casilla = r.nextInt(0,37);
     }
 
+    /**
+     * Getter de variable color
+     * @return la variable color
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     * Setter de variable color
+     */
     public void setColor() {
         int[] conjNegro={2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35};
-        for (int i = 0; i < conjNegro.length; i++) {
-            if (this.getCasilla()==conjNegro[i]){
-                this.color="Negro";
+        for (int negro : conjNegro) {
+            if (this.getCasilla() == negro) {
+                this.color = "Negro";
                 break;
-            } else if (this.getCasilla()==0) {
-                this.color="Verde";
+            } else if (this.getCasilla() == 0) {
+                this.color = "Verde";
                 break;
-            } else{
-                this.color="Rojo";
+            } else {
+                this.color = "Rojo";
             }
         }
     }
 
+    /**
+     * Asigna ambas variables
+     */
     public void toSpin(){
         this.setCasilla();
         this.setColor();
